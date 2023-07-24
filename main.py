ca74bec77a78fd33ff93864a6804f9c8afb8ac0b
Hossein Zalaghi <hossein73.z@gmail.com>
2023-07-24 18:29:52 +0330

    Send video link if upload fails

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 
 import instaloader
-import telegram.error
+from telegram.error import TimedOut, InvalidToken, BadRequest
 from instaloader import Post, ConnectionException
 from telegram import Update, InputMediaPhoto, InputMediaVideo
 from telegram.ext import Application, ContextTypes, MessageHandler, filters
@@ -24,19 +24,19 @@ async def process(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         progress = person.progress
 
-        # If the received message is password, Delete it for security reason
-        if progress["name"] == 'INS_REG' and progress["value"] == "INS_PASS":
-            try:
-                await context.bot.deleteMessage(chat_id=person.chat_id, message_id=update.message.id)
-            except Exception as e:
-                print('main: ' + red(str(e)))
-
         # Check user progress and get new message text from 'check_progress' function
         text = check_progress(progress, person, update)
         try:
             await context.bot.send_message(chat_id=person.chat_id, text=text)
         except Exception as e:
             print('main: ' + red(str(e)))
+
+        # If the received message is password, Delete it for security reason
+        if progress["name"] == 'INS_REG' and progress["value"] == "INS_PASS":
+            try:
+                await context.bot.deleteMessage(chat_id=person.chat_id, message_id=update.message.id)
+            except Exception as e:
+                print('main: ' + red(str(e)))
 
     else:  # -------------------- User have no unfinished progress
         re_match = re.match(
@@ -50,11 +50,26 @@ async def process(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             print(yellow("Shortcode: ") + group6)
 
             if person.session:  # -------------------- Check if user already logged in
+
                 loader = instaloader.Instaloader()
                 loader.load_session(username=person.insta_username, session_data=person.session)
+
+                post = None
                 try:
                     post = Post.from_shortcode(loader.context, shortcode=group6)
+                except ConnectionException:
+                    edit(Person, person.id, progress={"name": "INS_REG", "value": "INS_UNAME"}, session=None)
+                    await context.bot.send_message(chat_id=person.chat_id,
+                                                   text="There was somthing wrong with instagram server. "
+                                                        "you need to login again.\n"
+                                                        "please send your instagram username.")
+                except Exception as e:
+                    print('main: ' + red(str(e)))
 
+                loader.close()
+
+                slides = []
+                try:
                     if post.typename == "GraphSidecar":
                         # -------------------- Received link was an album
 
@@ -67,48 +82,59 @@ async def process(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                         ]
 
                         if slides:
-                            await context.bot.send_media_group(
-                                chat_id=person.chat_id,
-                                media=slides,
-                                reply_to_message_id=update.message.id,
-                                caption=post.caption)
+                            await context.bot.send_media_group(chat_id=person.chat_id,
+                                                               media=slides,
+                                                               reply_to_message_id=update.message.id,
+                                                               caption=post.caption
+                                                               )
 
                     elif post.typename == "GraphVideo":
                         # -------------------- Received link was a video
 
                         # Create media group from video and its thumbnail
                         slides = [InputMediaPhoto(media=post.url), InputMediaVideo(media=post.video_url)]
-                        await context.bot.send_media_group(
-                            chat_id=person.chat_id,
-                            media=slides,
-                            reply_to_message_id=update.message.id,
-                            caption=post.caption)
+                        await context.bot.send_media_group(chat_id=person.chat_id,
+                                                           media=slides,
+                                                           reply_to_message_id=update.message.id,
+                                                           caption=post.caption
+                                                           )
 
                     elif post.typename == "GraphImage":
                         # -------------------- Received link was a single photo
-                        await context.bot.send_photo(
-                            chat_id=person.chat_id,
-                            photo=post.url,
-                            reply_to_message_id=update.message.id,
-                            caption=post.caption)
+                        await context.bot.send_photo(chat_id=person.chat_id,
+                                                     photo=post.url,
+                                                     reply_to_message_id=update.message.id,
+                                                     caption=post.caption
+                                                     )
 
                     else:
                         # -------------------- Received link was an unknown post
                         print(post.typename)
+                except BadRequest as e:
+                    if "wrong type of the web page content" in e.message:
+                        media_url = ""
 
-                except ConnectionException:
-                    edit(Person, person.id, progress={"name": "INS_REG", "value": "INS_UNAME"}, session=None)
-                    await context.bot.send_message(
-                        chat_id=person.chat_id,
-                        text="There was somthing wrong with instagram server. "
-                             "you need to login again.\n"
-                             "please send your instagram username.")
+                        failed_msgs: list[str] = re.findall(r"#\d+", e.message)
+                        for failed_msg in failed_msgs:
+                            media_item = slides.pop(int(failed_msg.replace("#", "")) - 1)
+                            media_url = post.video_url if type(media_item) == InputMediaVideo else post.url
+
+                        text = "There was a problem uploading media to telegram. " \
+                               "use this link to download missing media:\n\n"
+                        text += media_url
+
+                        await context.bot.send_message(chat_id=person.chat_id,
+                                                       text=text,
+                                                       reply_to_message_id=update.message.id
+                                                       )
+                        await context.bot.send_media_group(chat_id=person.chat_id,
+                                                           media=slides,
+                                                           reply_to_message_id=update.message.id,
+                                                           caption=post.caption
+                                                           )
 
                 except Exception as e:
-                    print('main: ' + red(str(e)))
-
-                finally:
-                    loader.close()
+                    print(f'main: {red(str(e))}')
 
             else:
                 text = "To use this bot you must first login with your instagram account.\n" \
@@ -139,9 +165,9 @@ def main() -> None:
 
     try:
         application.run_polling()
-    except telegram.error.TimedOut as e:
+    except TimedOut as e:
         print(f'main: {red(str(e))}')
-    except telegram.error.InvalidToken as e:
+    except InvalidToken as e:
         print(f'main: {red(str(e))}')
         edit(Setting, id=setting.id, value=None)
 
